2b0824f97f2f5842e41a5d503177b51e49649cff
hueidou <hueidou163@gmail.com>
2016-05-19 15:12:09 +0800

    Log4NetPractice

---
+++ Log4NetPractice/Log4NetPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace Log4NetPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            log.Error("logger root");
+
+
+            var logA = log4net.LogManager.GetLogger("loggerA");
+            logA.Error("logger a");
+
+            var logB = log4net.LogManager.GetLogger("loggerB");
+            logB.Error("logger b");
+        }
+    }
+}
